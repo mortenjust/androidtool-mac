8dca6ba1bbcabe256f0191c5359d7cdc2f116fbb
mortenjust <morten.just@genstart.dk>
2015-04-22 15:51:15 -0700

    Initial Commit

---
+++ AndroidTool/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AndroidTool
+//
+//  Created by Morten Just Petersen on 4/22/15.
+//  Copyright (c) 2015 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
